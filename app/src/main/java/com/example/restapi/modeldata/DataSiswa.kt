9504d35f8402cf9dabc54c7cb6b2b2fd6ab3cc2b
MalkingMan <arrayalkhozini@gmail.com>
2025-12-18 09:13:40 +0700

    Add DataSiswa and UIStateSiswa data classes
    - Create `DataSiswa.kt` in `modeldata` package.
    - Define `DataSiswa` data class with serialization support.
    - Define `UIStateSiswa` data class for managing UI state.

---
+++ app/src/main/java/com/example/restapi/modeldata/DataSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.restapi.modeldata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id: Int,
+    val nama: String,val alamat: String,
+    val telpon: String
+)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
