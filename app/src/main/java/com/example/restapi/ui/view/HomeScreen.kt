ba45f92013530ad4a2cec7e7e906da502e82e631
MalkingMan <arrayalkhozini@gmail.com>
2025-12-20 08:23:57 +0700

    Add HomeScreen composable
    - Create `HomeScreen.kt` in `ui/view` package.
    - Define `HomeScreen` composable function with navigation support and ViewModel integration.
    - Add `LaunchedEffect` to refresh student data (`viewModel.getSiswa()`) when the screen is displayed.

---
+++ app/src/main/java/com/example/restapi/ui/view/HomeScreen.kt
@@ -0,0 +1,35 @@
+package com.example.restapi.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.restapi.R
+import com.example.restapi.modeldata.DataSiswa
+import com.example.restapi.ui.viewmodel.HomeViewModel
+import com.example.restapi.ui.viewmodel.PenyediaViewModel
+import com.example.restapi.ui.viewmodel.StatusUiSiswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    // Refresh data setiap kali screen ini ditampilkan
+    LaunchedEffect(Unit) {
+        viewModel.getSiswa()
+    }
