eaa583b85736a0228037694ed5ac4fc7813f2dcc
MalkingMan <arrayalkhozini@gmail.com>
2025-12-20 08:27:27 +0700

    Add HomeViewModel and StatusUiSiswa for UI state management
    - Create `HomeViewModel.kt` in the `ui/viewmodel` package.
    - Define `StatusUiSiswa` sealed interface with `Success`, `Error`, and `Loading` states.
    - Implement `HomeViewModel` to manage `siswaUiState`.
    - Add `getSiswa()` function to fetch data from `RepositoryDataSiswa` and update the UI state.

---
+++ app/src/main/java/com/example/restapi/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.restapi.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.restapi.modeldata.DataSiswa
+import com.example.restapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+import retrofit2.HttpException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var siswaUiState: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        getSiswa()
+    }
+
+    fun getSiswa() {
+        viewModelScope.launch {
+            siswaUiState = StatusUiSiswa.Loading
+            siswaUiState = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
