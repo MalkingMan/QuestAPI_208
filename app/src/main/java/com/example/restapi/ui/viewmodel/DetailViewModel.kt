4a82a07d3358d58ea5b685e6df165ce13d4f6170
MalkingMan <arrayalkhozini@gmail.com>
2026-01-01 12:46:19 +0700

    Add DetailViewModel and DetailUiState
    - Create `DetailViewModel.kt` in `ui/viewmodel` package.
    - Define `DetailUiState` sealed interface with `Success`, `Error`, and `Loading` states.

---
+++ app/src/main/java/com/example/restapi/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.restapi.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.restapi.modeldata.DataSiswa
+import com.example.restapi.modeldata.DetailSiswa
+import com.example.restapi.repositori.RepositoryDataSiswa
+import com.example.restapi.ui.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+sealed interface DetailUiState {
+    data class Success(val siswa: DetailSiswa) : DetailUiState
+    object Error : DetailUiState
+    object Loading : DetailUiState
+}
